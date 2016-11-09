16b0224d3ffce9f11820403117671bbbd350718d
remorhaz <zoohie@gmail.com>
2016-11-10 02:06:26 +0300

    Tests from json-patch/json-patch-tests used.

--- tests/PatchTest.php
+++ tests/PatchTest.php
@@ -14,6 +14,77 @@ class PatchTest extends \PHPUnit_Framework_TestCase
      * @param mixed $data
      * @param array $patchData
      * @param mixed $expectedData
+     * @dataProvider providerValidSpecPatch_Result
+     */
+    public function testApply_ValidSpecPatch_Applied($data, array $patchData, $expectedData)
+    {
+        $dataWriter = new RawSelectableWriter($data);
+        $patchDataReader = new RawSelectableReader($patchData);
+        (new Patch($dataWriter))->apply($patchDataReader);
+        $this->assertEquals($expectedData, $data);
+    }
+
+
+    public function providerValidSpecPatch_Result(): array
+    {
+        $dataSetList = [];
+        foreach ($this->getSpecTests() as $testInfo) {
+            if (isset($testInfo->error) || !isset($testInfo->expected)) {
+                continue;
+            }
+            $dataSet = [
+                $testInfo->doc,
+                $testInfo->patch,
+                $testInfo->expected,
+            ];
+            if (isset($testInfo->comment)) {
+                $dataSetList[$testInfo->comment] = $dataSet;
+            } else {
+                $dataSetList[] = $dataSet;
+            }
+        }
+        return $dataSetList;
+    }
+
+
+    /**
+     * @param mixed $data
+     * @param array $patchData
+     * @dataProvider providerInvalidPatch
+     * @expectedException \Exception
+     */
+    public function testApply_InvalidSpecPatch_ExceptionThrown($data, array $patchData)
+    {
+        $dataWriter = new RawSelectableWriter($data);
+        $patchDataReader = new RawSelectableReader($patchData);
+        (new Patch($dataWriter))->apply($patchDataReader);
+    }
+
+
+    public function providerInvalidSpecPatch(): array
+    {
+        $dataSetList = [];
+        foreach ($this->getSpecTests() as $testInfo) {
+            if (!isset($testInfo->error)) {
+                continue;
+            }
+            $dataSet = [
+                $testInfo->doc,
+                $testInfo->patch,
+            ];
+            if (isset($testInfo->comment)) {
+                $dataSetList[$testInfo->comment] = $dataSet;
+            } else {
+                $dataSetList[] = $dataSet;
+            }
+        }
+        return $dataSetList;
+    }
+
+    /**
+     * @param mixed $data
+     * @param array $patchData
+     * @param mixed $expectedData
      * @dataProvider providerValidPatch_Result
      */
     public function testApply_ValidPatch_Applied($data, array $patchData, $expectedData)
@@ -27,122 +98,27 @@ class PatchTest extends \PHPUnit_Framework_TestCase
 
     public function providerValidPatch_Result(): array
     {
-        return [
-            'A.1' => [
-                (object) ['foo' => 'bar'],
-                [
-                    (object) ['op' => 'add', 'path' => '/baz', 'value' => 'qux'],
-                ],
-                (object) ['baz' => 'qux', 'foo' => 'bar'],
-            ],
-            'A.2' => [
-                (object) ['foo' => ['bar', 'baz']],
-                [
-                    (object) ['op' => 'add', 'path' => '/foo/1', 'value' => 'qux'],
-                ],
-                (object) ['foo' => ['bar', 'qux', 'baz']],
-            ],
-            'A.3' => [
-                (object) ['baz' => 'qux', 'foo' => 'bar'],
-                [
-                    (object) ['op' => 'remove', 'path' => '/baz'],
-                ],
-                (object) ['foo' => 'bar'],
-            ],
-            'A.4' => [
-                (object) ['foo' => ['bar', 'qux', 'baz']],
-                [
-                    (object) ['op' => 'remove', 'path' => '/foo/1'],
-                ],
-                (object) ['foo' => ['bar', 'baz']],
-            ],
-            'A.5' => [
-                (object) ['baz' => 'qux', 'foo' => 'bar'],
-                [
-                    (object) ['op' => 'replace', 'path' => '/baz', 'value' => 'boo'],
-                ],
-                (object) ['baz' => 'boo', 'foo' => 'bar'],
-            ],
-            'A.6' => [
-                (object) [
-                    'foo' => (object) ['bar' => 'baz', 'waldo' => 'fred'],
-                    'qux' => (object) ['corge' => 'grault'],
-                ],
-                [
-                    (object) ['op' => 'move', 'from' => '/foo/waldo', 'path' => '/qux/thud'],
-                ],
-                (object) [
-                    'foo' => (object) ['bar' => 'baz'],
-                    'qux' => (object) ['corge' => 'grault', 'thud' => 'fred'],
-                ],
-            ],
-            'A.7' => [
-                (object) ['foo' => ['all', 'grass', 'cows', 'eat']],
-                [
-                    (object) ['op' => 'move', 'from' => '/foo/1', 'path' => '/foo/3'],
-                ],
-                (object) ['foo' => ['all', 'cows', 'eat', 'grass']],
-            ],
-            'A.10' => [
-                (object) ['foo' => 'bar'],
-                [
-                    (object) ['op' => 'add', 'path' => '/child', 'value' => (object) ['grandchild' => (object) []]],
-                ],
-                (object) ['foo' => 'bar', 'child' => (object) ['grandchild' => (object) []]],
-            ],
-            'A.11' => [
-                (object) ['foo' => 'bar'],
-                [
-                    (object) ['op' => 'add', 'path' => '/baz', 'value' => 'qux', 'xyz' => 123],
-                ],
-                (object) ['foo' => 'bar', 'baz' => 'qux'],
-            ],
-            'A.16' => [
-                (object) ['foo' => ['bar']],
-                [
-                    (object) ['op' => 'add', 'path' => '/foo/-', 'value' => ['abc', 'def']],
-                ],
-                (object) ['foo' => ['bar', ['abc', 'def']]],
-            ],
-        ];
+        $dataSetList = [];
+        foreach ($this->getTests() as $testInfo) {
+            if (isset($testInfo->error) || !isset($testInfo->expected)) {
+                continue;
+            }
+            $dataSet = [
+                $testInfo->doc,
+                $testInfo->patch,
+                $testInfo->expected,
+            ];
+            if (isset($testInfo->comment)) {
+                $dataSetList[$testInfo->comment] = $dataSet;
+            } else {
+                $dataSetList[] = $dataSet;
+            }
+        }
+        return $dataSetList;
     }
-
 
     /**
-     * @param $data
-     * @param array $patchData
-     * @dataProvider providerValidPatch_Success
-     */
-    public function testApply_ValidPatch_Success($data, array $patchData)
-    {
-        $dataWriter = new RawSelectableWriter($data);
-        $patchDataReader = new RawSelectableReader($patchData);
-        (new Patch($dataWriter))->apply($patchDataReader);
-    }
-
-
-    public function providerValidPatch_Success(): array
-    {
-        return [
-            'A.8' => [
-                (object) ['baz' => 'qux', 'foo' => ["a", 2, "c"]],
-                [
-                    (object) ['op' => 'test', 'path' => '/baz', 'value' => 'qux'],
-                    (object) ['op' => 'test', 'path' => '/foo/1', 'value' => 2],
-                ],
-            ],
-            'A.14' => [
-                (object) ['/' => 9, '~1' => 10],
-                [
-                    (object) ['op' => 'test', 'path' => '/~01', 'value' => 10],
-                ],
-            ],
-        ];
-    }
-
-
-    /**
-     * @param $data
+     * @param mixed $data
      * @param array $patchData
      * @dataProvider providerInvalidPatch
      * @expectedException \Exception
@@ -157,26 +133,46 @@ class PatchTest extends \PHPUnit_Framework_TestCase
 
     public function providerInvalidPatch(): array
     {
-        return [
-            'A.9' => [
-                (object) ['baz' => 'qux'],
-                [
-                    (object) ['op' => 'test', 'path' => '/baz', 'value' => 'bar'],
-                ],
-            ],
-            'A.12' => [
-                (object) ['foo' => 'bar'],
-                [
-                    (object) ['op' => 'add', 'path' => '/baz/bat', 'value' => 'qux'],
-                ],
-            ],
-            // A.13 cannot be tested.
-            'A.14' => [
-                (object) ['/' => 9, '~1' => 10],
-                [
-                    (object) ['op' => 'test', 'path' => '/~01', 'value' => "10"],
-                ],
-            ],
-        ];
+        $dataSetList = [];
+        foreach ($this->getTests() as $testInfo) {
+            if (!isset($testInfo->error)) {
+                continue;
+            }
+            $dataSet = [
+                $testInfo->doc,
+                $testInfo->patch,
+            ];
+            if (isset($testInfo->comment)) {
+                $dataSetList[$testInfo->comment] = $dataSet;
+            } else {
+                $dataSetList[] = $dataSet;
+            }
+        }
+        return $dataSetList;
+    }
+
+
+    private function getSpecTests(): array
+    {
+        $testsFile = dirname(realpath(PHPUNIT_COMPOSER_INSTALL)) . "/../tests/data/spec_tests.json";
+        return $this->getTestInfoList($testsFile);
+    }
+
+
+    private function getTests(): array
+    {
+        $testsFile = dirname(realpath(PHPUNIT_COMPOSER_INSTALL)) . "/../tests/data//tests.json";
+        return $this->getTestInfoList($testsFile);
+    }
+
+
+    private function getTestInfoList($fileName): array
+    {
+        $testInfoListJSON = file_get_contents($fileName);
+        $testInfoList = json_decode($testInfoListJSON);
+        $isNotDisabledTest = function(\stdClass $testInfo) {
+            return !(isset($testInfo->disabled) && $testInfo->disabled);
+        };
+        return array_filter($testInfoList, $isNotDisabledTest);
     }
 }
